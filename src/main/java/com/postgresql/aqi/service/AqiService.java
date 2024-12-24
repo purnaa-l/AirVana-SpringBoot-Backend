99298edb8698e3011fe516f549524a0133fa4213
Purnaa <147434929+purnaa-l@users.noreply.github.com>
2024-12-25 00:02:40 +0530

    Refactored

--- src/main/java/com/postgresql/aqi/service/AqiService.java
+++ src/main/java/com/postgresql/aqi/service/AqiService.java
@@ -1,7 +1,6 @@
 package com.postgresql.aqi.service;
 
 import com.postgresql.aqi.dto.AqiDto;
-import com.postgresql.aqi.entity.Aqi;
 
 public interface AqiService {
     AqiDto createAqi(AqiDto aqiDto);
