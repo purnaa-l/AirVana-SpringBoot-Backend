c191010b5a15823417ad4ebecb8e66a74e5bac16
Purnaa <147434929+purnaa-l@users.noreply.github.com>
2025-01-11 13:58:30 +0530

    feat: API logs backend (#11)

--- src/main/java/com/postgresql/aqi/service/impl/ApiLogService.java
+++ src/main/java/com/postgresql/aqi/service/impl/ApiLogService.java
@@ -1,16 +1,39 @@
+//package com.postgresql.aqi.service.impl;
+//
+//import com.postgresql.aqi.entity.ApiLog;
+//import com.postgresql.aqi.repository.ApiLogRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import java.time.LocalDateTime;
+//
+//@Service
+//public class ApiLogService {
+//
+//    @Autowired
+//    private ApiLogRepository apiLogRepository;
+//
+//
+//}
 package com.postgresql.aqi.service.impl;
 
 import com.postgresql.aqi.entity.ApiLog;
 import com.postgresql.aqi.repository.ApiLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ApiLogService {
 
     @Autowired
     private ApiLogRepository apiLogRepository;
+
+    // Fetch all logs from the database
+    public List<ApiLog> getAllApiLogs() {
+        return apiLogRepository.findAll();
+    }
 
     public void logApiRequest(String endpoint, Long userId, Long responseTime, int statusCode) {
         ApiLog log = new ApiLog();
