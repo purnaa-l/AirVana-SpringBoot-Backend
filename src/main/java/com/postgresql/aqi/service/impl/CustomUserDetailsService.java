9af48ee852ee58e4ad4e61d2ec934eda4a23adf0
purnaa-l <mynameispurnaa@gmail.com>
2025-05-09 00:56:25 +0530

    feat: Spring Security

---
+++ src/main/java/com/postgresql/aqi/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.postgresql.aqi.service.impl;
+
+import com.postgresql.aqi.entity.User;
+import com.postgresql.aqi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
